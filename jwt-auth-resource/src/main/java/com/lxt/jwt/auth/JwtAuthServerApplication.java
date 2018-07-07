36a85ba5b2d90f8e2fc7556a4fda122f6df94a55
zer0Black <lxtalx@gmail.com>
2018-07-07 19:48:09 +0800

    [添加]jwt校验的联系

---
+++ jwt-auth-resource/src/main/java/com/lxt/jwt/auth/JwtAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.lxt.jwt.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtAuthServerApplication.class, args);
+    }
+
+}
---
+++ jwt-auth-resource/src/main/java/com/lxt/jwt/resource/JwtReroucesServerApplication.java
@@ -0,0 +1,13 @@
+package com.lxt.jwt.resource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtReroucesServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtReroucesServerApplication.class, args);
+    }
+
+}
